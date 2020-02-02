553e118053c5308634c9506cc832af73995c971c
Divya Kamat <divyakamat96@gmail.com>
2020-02-01 17:22:46 -0800

    Implemented functionality for the two sub projects

--- json-manipulation/src/main/java/ai/brace/Main.java
+++ json-manipulation/src/main/java/ai/brace/Main.java
@@ -1,9 +1,113 @@
 package ai.brace;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, world.");
+import com.google.gson.*;
+import java.io.*;
+import java.util.*;
+
+/***
+ * Data class stores ids and textdata from the JSON files
+ */
+class Data {
+    int id;
+    String txtdata;
+
+    public Data(int id, String txtdata) {
+        this.id = id;
+        this.txtdata = txtdata;
+    }
+}
+
+
+public class Main {
+
+    /**
+     * HashMap to store the words from textdata and their counts
+     */
+    static Map wordCount = new HashMap<String, Integer>();
+
+    public static void main(String[] args) {
+        List<Data> dataList = new ArrayList<Data>();
+
+        // Task 1
+        System.out.println("\n\nTASK 1");
+        String file1 = "src/main/resources/a1.json";
+        dataList = addToList(dataList, file1);
+        sortData(dataList);
+        printOutput(dataList);
+
+        // Task 2
+        System.out.println("\n\nTASK 2");
+        String file2 = "src/main/resources/a2.json";
+        dataList = addToList(dataList, file2);
+        printOutput(dataList);
+
+        //Task 3
+        System.out.println("\n\nTASK 3");
+        System.out.println(wordCount);
+
+    }
+
+    /**
+     * Comparator to sort textdata sentences in dataList
+     * @param dataList
+     */
+    public static void sortData(List<Data> dataList) {
+        Collections.sort(dataList, new Comparator<Data>() {
+            public int compare(Data d1, Data d2) {
+                return d1.id - d2.id;
+            }
+        });
+    }
+
+    /**
+     * Prints the textdata from list of Data class
+     * @param dataList
+     */
+    public static void printOutput(List<Data> dataList) {
+        for (Data d : dataList)
+            System.out.println(d.txtdata);
+    }
+
+    /**
+     * Adds ids and textdata from the JsonArray to datalist
+     * @param dataList
+     * @param file
+     * @return returns list containing data parsed from JSON file
+     */
+    public static List<Data> addToList(List<Data> dataList, String file) {
+
+        Gson gson = new Gson();
+
+        JsonParser jsonParser = new JsonParser();
+
+        try {
+            JsonElement jsonTree = jsonParser.parse(new FileReader(file));
+
+            JsonObject jsonObject = jsonTree.getAsJsonObject();
+
+            JsonArray array = jsonObject.getAsJsonArray("textArray");
+
+            array.forEach((element) ->
+            {
+                if (element.isJsonObject()) {
+
+                    int id = element.getAsJsonObject().get("id").getAsInt();
+                    String txt = element.getAsJsonObject().get("textdata").getAsString();
+
+                    //Adds words and counts to hashmap
+                    for (String s : txt.split(" ")) {
+                        int count = (int) wordCount.getOrDefault(s, 0);
+                        wordCount.put(s, count + 1);
+                    }
+
+                    dataList.add(new Data(id, txt));
+
+                }
+            });
+        } catch (Exception e) {
+            //use logging framework here to log any exceptions
+        }
+
+        return dataList;
     }
 }
--- mystery-duplicates/src/main/java/ai/brace/Employee.java
+++ mystery-duplicates/src/main/java/ai/brace/Employee.java
@@ -9,9 +9,45 @@ public class Employee
 
     public Employee(String _lastName, String _firstName, String _middleInitial, String _socialSecurityNumber)
     {
-        firstName = _firstName;
-        middleInitial = _middleInitial;
-        lastName = _lastName;
-        socialSecurityNumber = _socialSecurityNumber;
+        this.firstName = _firstName;
+        this.middleInitial = _middleInitial;
+        this.lastName = _lastName;
+        this.socialSecurityNumber = _socialSecurityNumber;
+    }
+
+    /**
+     *  Overrides equals methods for Employee object comparison
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(this == obj)
+            return true;
+
+        if(obj == null || obj.getClass()!= this.getClass())
+            return false;
+
+        Employee e = (Employee)obj;
+
+        return (e.firstName.equals(this.firstName) &&
+                e.middleInitial.equals(this.middleInitial) &&
+                e.lastName.equals(this.lastName) &&
+                e.socialSecurityNumber.equals(this.socialSecurityNumber));
+    }
+
+    /**
+     * Returns unique hashcode for Employee objects
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + this.firstName.hashCode();
+        result = 31 * result + this.middleInitial.hashCode();
+        result = 31 * result + this.lastName.hashCode();
+        result = 31 * result + this.socialSecurityNumber.hashCode();
+        return result;
     }
 }
